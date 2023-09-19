ed1516d0c270ae27f31a7d2af6fe1af0cd9802ad
ibrahim Salis <ibrahimsalisu@gmail.com>
2023-09-19 12:13:31 +0100

    ibb

---
+++ react-projects/react-one/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { useState } from 'ibb'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    {/* <ibb/> */}
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+    </>
+  )
+}
+
+export default App
+
---
+++ react-projects/react-one/src/Component/ibb.jsx
@@ -0,0 +1,13 @@
+import ''
+import App from '../App'
+function (ibb) {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>principal</p>
+    </>
+  )
+}
+
+export default App
